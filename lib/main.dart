e2748073d6b45131f868b5eefbf3075a856d2239
Yazhini Malar Pari <yazhinimalarpari@Yazhinis-MacBook-Air.local>
2023-06-06 13:23:06 +0530

    reddit json

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
-import './views/home_page.dart';
+import './models/reddit.dart';
+import './models/api.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  List<Post> _posts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getReddit().then((posts) {
+      setState(() {
+        _posts = posts;
+      });
+    });
+  }
+
+  List<Widget> buildListTiles() {
+    return _posts
+        .map((post) => ListTile(
+              leading: CircleAvatar(
+                  child: Image.network(
+                '${!post.thumbnail.contains(".jpg") ? "http://via.placeholder.com/300" : post.thumbnail}',
+                scale: 0.2,
+              )),
+              title: Text('Title: ${post.title} by ${post.author}'),
+              subtitle: Text(
+                'Subreddit: ${post.subreddit} with ${post.ups} upvotes',
+              ),
+            ))
+        .toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reddit Top'),
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        children: buildListTiles(),
+      ),
     );
   }
 }
---
+++ lib/models/serializers.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'serializers.dart';
+
+// **************************************************************************
+// BuiltValueGenerator
+// **************************************************************************
+
+Serializers _$serializers = (new Serializers().toBuilder()
+      ..add(Children.serializer)
+      ..add(Data.serializer)
+      ..add(Post.serializer)
+      ..add(Reddit.serializer)
+      ..addBuilderFactory(
+          const FullType(BuiltList, const [const FullType(Data)]),
+          () => new ListBuilder<Data>()))
+    .build();
+
+// ignore_for_file: deprecated_member_use_from_same_package,type=lint
